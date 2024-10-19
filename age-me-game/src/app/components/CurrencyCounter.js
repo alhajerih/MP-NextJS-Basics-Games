e2359aeefdea10977fb9ffdb0c862d1dd747caf6
alhajerih <alhajerih@hotmail.com>
2024-10-19 16:44:08 +0300

    Waiting for review

--- age-me-game/src/app/components/CurrencyCounter.js
+++ age-me-game/src/app/components/CurrencyCounter.js
@@ -4,7 +4,11 @@ import React from "react";
 import { useState } from "react";
 
 function CurrencyCounter({ netWorth }) {
-  return <div className="self-center"><p className="currency-text">{netWorth}</p></div>;
+  return (
+    <div className="self-center">
+      <p className="currency-text">{netWorth}</p>
+    </div>
+  );
 }
 
 export default CurrencyCounter;
