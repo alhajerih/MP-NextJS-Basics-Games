6c219addb4cb503f98caa363ba2c7cd4917b5686
jannakam <jannakalmuqaisib@gmail.com>
2024-10-20 09:34:04 +0300

    adding animation to currency

--- age-me-game/src/app/components/CurrencyCounter.js
+++ age-me-game/src/app/components/CurrencyCounter.js
@@ -1,12 +1,20 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 function CurrencyCounter({ netWorth }) {
+  // Create a spring for netWorth value
+  const props = useSpring({
+    value: netWorth,
+    config: { tension: 170, friction: 26 },
+  });
+
   return (
     <div className="self-center">
-      <p className="currency-text">{netWorth}KD</p>
+      <animated.p className="currency-text">
+        {props.value.to((val) => `${Math.floor(val)} KD`)}
+      </animated.p>
     </div>
   );
 }
